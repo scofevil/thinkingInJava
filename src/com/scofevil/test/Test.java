6502c2c01a3cabe8482029764e2f55f8db068cfa
luhaifeng <luhaifeng.sh@superjia.com>
2016-08-23 10:03:09 +0800

    ConcurrentModificationException

--- src/com/scofevil/test/Test.java
+++ src/com/scofevil/test/Test.java
@@ -29,7 +29,12 @@ public class Test {
         list.add(new Test("1"));
         list.add(new Test("2"));
         list.add(new Test("3"));
+        list.add(new Test("4"));
+        list.add(new Test("5"));
         Iterator<Test> iterator = list.iterator();
+        Test[] tmp2 = { new Test("t") };
+        Object[] tmp = list.toArray();
+        Test[] tmp1 = list.toArray(tmp2);
         Consumer<Test> action = new Consumer<Test>() {
             public void accept(Test t) {
                 t.setT("0");
@@ -37,12 +42,13 @@ public class Test {
                 t = null;
             }
         };
-        //        iterator.forEachRemaining(action);
-        System.out.println(list);
+        iterator.forEachRemaining(action);
+        System.out.println(tmp);
+        System.out.println(tmp1);
         while (iterator.hasNext()) {
             Test t = iterator.next();
             System.out.println(t);
-            list.remove(t);
+            //            list.remove(t);
         }
     }
 
