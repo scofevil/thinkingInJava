b251fc0400443af8592f42fda6c67f8bef3d4c64
luhaifeng <luhaifeng.sh@superjia.com>
2016-08-30 18:46:45 +0800

    invocation

---
+++ src/com/scofevil/test/Subject.java
@@ -0,0 +1,5 @@
+package com.scofevil.test;
+
+public interface Subject {
+    public void dealTask(String taskName);
+}
---
+++ src/com/scofevil/test/TestThreadLocal.java
@@ -0,0 +1,7 @@
+package com.scofevil.test;
+
+public class TestThreadLocal {
+    public static void main(String[] args) {
+        ThreadLocal tl = new ThreadLocal<Integer>();
+    }
+}
