53399636c964d5099a282b9b96a956070a9d2787
luhaifeng <luhaifeng.sh@superjia.com>
2016-09-07 10:55:59 +0800

    2

---
+++ src/com/scofevil/test/T.java
@@ -0,0 +1,22 @@
+package com.scofevil.test;
+
+public class T<T> {
+    static int i = 1;
+    T          t;
+    static {
+        i = 2;
+    }
+
+    public T() {
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("1");
+    }
+
+    public static void main(String[] args) {
+        System.out.println(t);
+    }
+}
