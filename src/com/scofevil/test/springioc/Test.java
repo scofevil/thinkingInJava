7dacab751d50b9c789607807e40fb3aaf1e44e6b
luhaifeng <luhaifeng.sh@superjia.com>
2016-09-13 17:17:34 +0800

    fix pom

--- src/com/scofevil/test/springioc/Test.java
+++ src/com/scofevil/test/springioc/Test.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class Test {
+    @SuppressWarnings("unused")
     private static UserDaoI userDaoI;
 
+    @SuppressWarnings("resource")
     public static void main(String[] args) {
         BeanFactory factory = new FileSystemXmlApplicationContext("classpath:ioc-test.xml");
         UserManager userManager = (UserManager) factory.getBean("userManager");
