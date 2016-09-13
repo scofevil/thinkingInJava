ea3388877697d0a317c2e59d28b51e8f614e95f9
luhaifeng <luhaifeng.sh@superjia.com>
2016-09-13 17:39:42 +0800

    lombok

--- src/com/scofevil/test/springioc/Test.java
+++ src/com/scofevil/test/springioc/Test.java
@@ -1,8 +1,11 @@
 package com.scofevil.test.springioc;
 
+import lombok.Data;
+
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
+@Data
 public class Test {
     @SuppressWarnings("unused")
     private static UserDaoI userDaoI;
