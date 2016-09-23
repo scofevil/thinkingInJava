e6213e6f459537d28e5c91c01367c975e0259115
luhaifeng <luhaifeng.sh@superjia.com>
2016-09-23 11:15:28 +0800

    1

--- src/com/scofevil/test/construct/SubClass.java
+++ src/com/scofevil/test/construct/SubClass.java
@@ -3,6 +3,7 @@ package com.scofevil.test.construct;
 public class SubClass extends SuperClass {
     SubClass(String str) {
         super(str);
+        this.name = "";
         System.out.println("Sub with string");
     }
 
@@ -15,6 +16,7 @@ public class SubClass extends SuperClass {
 class SuperClass {
     //SuperClass() {
     //}
+    String name;
 
     SuperClass(String str) {
         System.out.println("Super with a string");
