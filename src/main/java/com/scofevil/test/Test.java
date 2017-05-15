9778944d2df13d4e115bc2acb46ff86471ee1b27
luhaifeng <luhaifeng.sh@superjia.com>
2017-05-15 18:04:19 +0800

    fxi

--- src/main/java/com/scofevil/test/Test.java
+++ src/main/java/com/scofevil/test/Test.java
@@ -1,14 +1,12 @@
 package com.scofevil.test;
 
-import java.util.EnumMap;
-
 import com.scofevil.test.enumt.Color;
 
 public class Test {
     public static void main(String[] args) throws Throwable {
 
-        EnumMap<Color, String> map = new EnumMap<Color, String>(Color.class);
-        map.put(Color.GREEN, "green");
-        System.out.println(map);
+        Color.RED.test();
+        Color.YELLOW.test();
+        System.out.println(Color.YELLOW.getName());
     }
 }
--- src/main/java/com/scofevil/test/enumt/Color.java
+++ src/main/java/com/scofevil/test/enumt/Color.java
@@ -3,13 +3,25 @@ package com.scofevil.test.enumt;
 public enum Color {
     RED("red", 1),
     GREEN("green", 2),
-    YELLOW("yellow", 3);
+    YELLOW {
+        public void test() {
+            System.out.println("yellow");
+        }
+    };
     private String name;
     private int    id;
+
+    public void test() {
+        System.out.println(this.name);
+    }
 
     Color(String name, int id) {
         this.name = name;
         this.id = id;
+    }
+
+    Color() {
+
     }
 
     public String getName() {
