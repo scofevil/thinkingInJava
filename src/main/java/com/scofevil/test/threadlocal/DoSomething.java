c65cccff526619f7cc7f48418dedd63f45807a39
luhaifeng <luhaifeng.sh@superjia.com>
2017-06-16 17:57:03 +0800

    fxi

---
+++ src/main/java/com/scofevil/test/threadlocal/DoSomething.java
@@ -0,0 +1,36 @@
+package com.scofevil.test.threadlocal;
+
+public class DoSomething implements Runnable {
+
+    private static ThreadLocal<String> name  = new ThreadLocal<String>() {
+                                                 @Override
+                                                 protected String initialValue() {
+                                                     return Thread.currentThread().getName();
+                                                 }
+                                             };
+    private static ThreadLocal<String> name1 = new ThreadLocal<String>() {
+                                                 @Override
+                                                 protected String initialValue() {
+                                                     return Thread.currentThread().getName();
+                                                 }
+                                             };
+    private static ThreadLocal<String> name2 = new ThreadLocal<String>() {
+                                                 @Override
+                                                 protected String initialValue() {
+                                                     return Thread.currentThread().getName();
+                                                 }
+                                             };
+
+    //    private String name = Thread.currentThread().getName();
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 4; i++) {
+            Thread t = Thread.currentThread();
+            System.out.println(t);
+            System.out.println(name.get() + " " + +i);
+            System.out.println(name1.get() + " " + +i);
+            System.out.println(name2.get() + " " + +i);
+        }
+    }
+}
---
+++ src/main/java/com/scofevil/test/threadlocal/ThreadLocalTest.java
@@ -0,0 +1,13 @@
+package com.scofevil.test.threadlocal;
+
+public class ThreadLocalTest {
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new DoSomething());
+        Thread t2 = new Thread(new DoSomething());
+        Thread t3 = new Thread(new DoSomething());
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
