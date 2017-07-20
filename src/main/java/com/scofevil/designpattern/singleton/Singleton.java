56c21ab0e1727b5978e8e1a442c919058c1c0adf
luhaifeng <luhaifeng.sh@superjia.com>
2017-07-20 18:26:40 +0800

    add designpattern

---
+++ src/main/java/com/scofevil/designpattern/singleton/Singleton.java
@@ -0,0 +1,25 @@
+package com.scofevil.designpattern.singleton;
+
+/**
+ * @param
+ * @author haifeng.lu
+ * @see
+ * @since 1.0
+ */
+public class Singleton {
+
+    //一个静态的实例
+    private static Singleton singleton;
+
+    //私有化构造函数
+    private Singleton(){}
+
+    //给出一个公共的静态方法返回一个单一实例
+    public static Singleton getSingleton(){
+        if (singleton==null){
+            singleton = new Singleton();
+        }
+        return singleton;
+    }
+
+}
---
+++ src/main/java/com/scofevil/designpattern/singleton/TestSingleton.java
@@ -0,0 +1,55 @@
+package com.scofevil.designpattern.singleton;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @param
+ * @author haifeng.lu
+ * @see
+ * @since 1.0
+ */
+public class TestSingleton {
+
+    private  boolean lock;
+
+
+    public boolean isLock() {
+        return lock;
+    }
+
+    public void setLock(boolean lock) {
+        this.lock = lock;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        final Set<String> instanceSet = Collections.synchronizedSet(new HashSet<String>());
+        final TestSingleton lock = new TestSingleton();
+        lock.setLock(true);
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for(int i=0;i<10;i++){
+            executorService.execute(new Runnable() {
+                @Override
+                public void run() {
+                    while (true){
+                        if(!lock.isLock()){
+                            Singleton singleton = Singleton.getSingleton();
+                            instanceSet.add(singleton.toString());
+                        }
+                    }
+                }
+            });
+        }
+        Thread.sleep(5000);
+        lock.setLock(false);
+        Thread.sleep(5000);
+        System.out.println("---------并发情况下我们取到的实例----------");
+        for(String each:instanceSet){
+            System.out.println(each);
+        }
+        executorService.shutdown();
+    }
+}
