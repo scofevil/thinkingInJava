3a6f37ea9eaa96962cb072eb18d6b20431780fae
scofevil <scofevil@gmail.com>
2017-08-13 21:52:30 +0800

    fix

--- src/main/java/com/scofevil/test/Test.java
+++ src/main/java/com/scofevil/test/Test.java
@@ -1,19 +1,26 @@
 package com.scofevil.test;
 
-import com.scofevil.test.enumt.Color;
+import lombok.AllArgsConstructor;
 
+import java.util.concurrent.ConcurrentHashMap;
+
+@AllArgsConstructor
 public class Test {
     public static void main(String[] args) throws Throwable {
-
-        Color.RED.test();
-        Color.YELLOW.test();
-        Color.GREEN.test();
-        Color.GREEN.test();
-
-        System.out.println(Color.YELLOW.getName());
-    }
-
-    public void test() {
+        ConcurrentHashMap<String, String> cm = new ConcurrentHashMap<>();
+        for (int i = 0; i < 14; i++) {
+            cm.put("key_" + i, "value_" + i);
+        }
+        test(1, 2);
 
     }
+
+    static void test(int aaaaaaa, int bbbbbb) {
+        aaaaaaa = aaaaaaa + 2;
+        int ccccccccc = aaaaaaa + bbbbbb;
+        System.out.println(ccccccccc);
+        System.out.println(aaaaaaa);
+        System.out.println(bbbbbb);
+    }
+
 }
