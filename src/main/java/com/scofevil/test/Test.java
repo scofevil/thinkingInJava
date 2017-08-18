80bb84d00c37fd01fcbf62ce96a0330f360dcd0e
scofevil <scofevil@gmail.com>
2017-08-18 15:34:45 +0800

    fix

--- src/main/java/com/scofevil/test/Test.java
+++ src/main/java/com/scofevil/test/Test.java
@@ -1,25 +1,23 @@
 package com.scofevil.test;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Test {
 
     static volatile boolean flag = false;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
         Thread t = new Thread(() -> {
-            while (!Thread.currentThread().isInterrupted()) {
-                System.out.println("im running");
-                System.out.println(Thread.currentThread().isInterrupted());
-                System.out.println("ending");
-            }
-            System.out.println("jieshu " + Thread.currentThread().isInterrupted());
-        });
-        t.start();
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-
-        }
+            System.out.println("thread start");
+            System.out.println("thread end");
+        },"tttttt");
+        executorService.execute(t);
+        t.join();
+        Thread.sleep(3000);
         t.interrupt();
+        System.out.println("end");
     }
 
 }
