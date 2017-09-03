391c55433d9e5fe2b34a759cc1ef4ba7d5c1e472
scofevil <scofevil@gmail.com>
2017-09-03 16:48:42 +0800

    fix

--- src/main/java/com/scofevil/test/Test.java
+++ src/main/java/com/scofevil/test/Test.java
@@ -9,19 +9,12 @@ import java.util.concurrent.locks.ReentrantLock;
 public class Test {
 
     private static ReentrantLock lock = new ReentrantLock();
-    public static void main(String[] args) throws InterruptedException {
-        List<Integer> li = new ArrayList<>();
-        li.add(1);
-        li.add(2);
-        System.out.println(li.hashCode());
-        List<Integer> tmp = li;
-        System.out.println(tmp);
 
-        li = new ArrayList<>();
-        System.out.println(li.hashCode());
-        li.add(3);
-        li.add(4);
-        System.out.println(tmp);
+    public static void main(String[] args) throws InterruptedException {
+        int i = 0;
+        int a = 1;
+        int b = 2;
+        System.out.println(i++ + ++i);
     }
 
 }
